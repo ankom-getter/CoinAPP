fe10501f20388b3e607ad497dda177ecb07912e2
Ruslan Voinarovskyi <economy.savvier_0q@icloud.com>
2023-03-05 23:00:08 +0100

    Initial Commit

---
+++ CoinAPP/CoinAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAPPApp.swift
+//  CoinAPP
+//
+//  Created by Ruslan Voinarovskyi on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
